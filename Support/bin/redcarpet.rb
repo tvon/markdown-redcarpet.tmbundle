b101e5134fe1257d4024b159345daf52fffb71ee
Jim Meyer <jim@geekdaily.org>
2014-05-25 11:31:13 -0500

    Put the proper commands into the warning dialog.

--- Support/bin/redcarpet.rb
+++ Support/bin/redcarpet.rb
@@ -25,19 +25,10 @@ rescue LoadError
   <div class="error">
   <h2>Please install the following gems on your system Ruby</h2>
 
-  <h3>Step 1: Install the required gems</h3>
   <pre><code>
-  sudo gem install redcarpet -v 2.3.0
-  sudo gem install pygments.rb
+  sudo /System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/gem install redcarpet -v 2.3.0
+  sudo /System/Library/Frameworks/Ruby.framework/Versions/1.8/usr/bin/gem install pygments.rb
   </code></pre>
-
-  <div class="hint">
-  <h3>WARNING: If you're using a Ruby version manager activate the system Ruby first</h3>
-  <pre><code>
-  rvm use system                          # for RVM
-  expoprt RBENV_VERSION="system"          # for rbenv
-  </code></pre>
-  </div>
 
   </div>
   HTML
